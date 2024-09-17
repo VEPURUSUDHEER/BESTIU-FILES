1bdab45bc764b30e0fb34a1461d86b59c501d527
sudhe <sudhe@sudheer>
2024-09-17 17:00:45 +0530

    THIRD COMMIT

---
+++ Javafiles/src/practice/ExpicitDemo1.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class ExpicitDemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a=9.87d;
+		int b=(int) a;
+	 System.out.println(a);		
+	 System.out.println(b)
+		;
+
+	}
+
+}
