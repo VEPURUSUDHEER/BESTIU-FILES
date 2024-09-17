b639d539552daf1f2d203eb167441bcc097c18e4
sudhe <sudhe@sudheer>
2024-09-17 16:49:53 +0530

    third commit

---
+++ Javafiles/src/practice/ImplicitDemo2.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class ImplicitDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float f=22.14f;
+		double d=f;
+		System.out.println(d);
+
+	}
+
+}
