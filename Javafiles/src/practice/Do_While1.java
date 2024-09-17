f39f62ca414e55cb81a2455375e68d11419e2af0
sudhe <sudhe@sudheer>
2024-09-17 17:54:39 +0530

    fifth commit

---
+++ Javafiles/src/practice/Do_While1.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class Do_While1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=1;
+	    do 
+	       {
+			System.out.println("a value is:" +a);
+			a++;
+		   }
+	    while(a<=10);
+
+	}
+
+}
