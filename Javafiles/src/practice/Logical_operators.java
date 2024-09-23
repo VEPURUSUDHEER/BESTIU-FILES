db8216ebbe1400b5ccad2de9d17870d74d01595c
sudhe <sudhe@sudheer>
2024-09-23 15:50:03 +0530

    eight commit

---
+++ Javafiles/src/practice/Logical_operators.java
@@ -0,0 +1,18 @@
+package practice;
+
+public class Logical_operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5,b=6,c=7;
+		//logical AND->&&
+		System.out.println("(a<b) && (b<c):" +((a<b) && (b<c)) );
+		//logical OR->||
+		System.out.println("(a>b) || (b<c):" +((a>b) || (b<c)) );
+		//logical NOT->!
+		System.out.println("!(b>c):" + !(b>c));
+		
+
+	}
+
+}
