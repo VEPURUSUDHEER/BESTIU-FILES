572cfafeb048dd9355274f8f503a0e86a72b0f7d
sudhe <sudhe@sudheer>
2024-10-12 11:45:29 +0530

    nineth commit

---
+++ Javafiles/src/Jr_developer/Kia_Airbags.java
@@ -0,0 +1,8 @@
+package Jr_developer;
+
+public class Kia_Airbags {
+	public void display() {
+		System.out.println("ALL KIA AIRBAGS");
+	}
+
+}
---
+++ Javafiles/src/jr1_developer/KIA_SPEED_LINES.java
@@ -0,0 +1,8 @@
+package jr1_developer;
+
+public class KIA_SPEED_LINES {
+	public void display() {
+		System.out.println("ALL KIA HAS POWER LIMITS");
+	}
+
+}
